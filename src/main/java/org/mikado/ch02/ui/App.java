4e0973e4d441ae4158c47908203e7d82c989c476
Carlos V. <cevabarcvilla@gmail.com>
2025-04-08 01:22:58 -0300

    Mikado chapter 2 - Launcher app

---
+++ src/main/java/org/mikado/ch02/ui/App.java
@@ -0,0 +1,20 @@
+package org.mikado.ch02.ui;
+
+public class App {
+
+    private UI ui;
+    private static String storePath;
+
+    public void launch() throws ApplicationException {
+        ui = new UI();
+        ui.showLogin();
+    }
+
+    public static String getStorageFile() {
+        return storePath;
+    }
+
+    public static void setStorageFile(String storePath) {
+        App.storePath = storePath;
+    }
+}
